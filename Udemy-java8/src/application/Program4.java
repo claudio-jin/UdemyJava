00559eebb61d2c5b364fb336a49a1bb2fdea1901
Claudio Jin <natsu165@hotmail.com>
2022-05-04 16:45:34 -0300

    pastas java7, java8, java9 criadas

---
+++ Udemy-java8/src/application/Program4.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program4 {
+
+	public static void main(String[] args) {
+		String[] lines = new String[] {"Good morning", "Good afternoon", "Good night"};
+		
+		String path = "d:\\in.txt";
+		
+		//Add o parâmetro true, faz com que o programa não crie um novo arquivo,
+		//ele acrescenta o que passa
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))) {
+			for (String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
